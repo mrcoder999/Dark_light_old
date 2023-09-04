2b575553f8885861d1c5b76891d52ef2b25603e3
mrcoder999 <119610550+mrcoder999@users.noreply.github.com>
2023-09-04 10:46:00 +0530

    Update color-toggle.js

--- javascripts/discourse/initializers/color-toggle.js
+++ javascripts/discourse/initializers/color-toggle.js
@@ -8,6 +8,19 @@ import cookie from "discourse/lib/cookie";
 import { observes } from "discourse-common/utils/decorators";
 import Session from "discourse/models/session";
 
+import { registerHelper } from "discourse-common/lib/helpers";
+
+registerHelper("media-helper", function ([value]) {
+  switch (value) {
+    case "dark":
+      return "all";
+    case "light":
+      return "none";
+    default:
+      return "(prefers-color-scheme: dark)";
+  }
+});
+
   const lightScheme = document.querySelector("link.light-scheme");
   const darkScheme =
     document.querySelector("link.dark-scheme") ||
